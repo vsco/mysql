f18ec1e93c65a128fb33ca21e050aab1ae6089b6
David Nghiem <defn@vsco.co>
2014-10-15 14:26:17 -0700

    eliminate duplicate mysql directory resources

--- recipes/server.rb
+++ recipes/server.rb
@@ -97,9 +97,8 @@ unless platform_family?(%w{mac_os_x})
   [File.dirname(node['mysql']['pid_file']),
     File.dirname(node['mysql']['tunable']['slow_query_log']),
     node['mysql']['confd_dir'],
-    node['mysql']['confd_dir'],
     node['mysql']['log_dir'],
-    node['mysql']['data_dir']].each do |directory_path|
+    node['mysql']['data_dir']].sort.uniq.each do |directory_path|
     directory directory_path do
       owner "mysql" unless platform? 'windows'
       group "mysql" unless platform? 'windows'
